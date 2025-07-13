8e02ae15b4dfbf91fd053ad32e0f33e507058b27
edicsonjohan <edicsonjohanlopez@gmail.com>
2025-07-13 15:11:57 -0500

    Agregadas imágenes al folder public y actualizado Missql.jsx

--- src/components/Missql.jsx
+++ src/components/Missql.jsx
@@ -6,25 +6,25 @@ const Missql = () => {
       <h1>EJERCICIOS MER</h1>
       <p>Ejercicio Zoologico</p>
       <img
-        src="/public/ZOOLOGICO.png"
+        src="/ZOOLOGICO.png"
         alt="ZOOLOGICO"
         style={{ width: "100%", maxWidth: "800px", marginBottom: "2rem" }}
       />
       <p>Ejercicio Abogados</p>
       <img
-        src="/public/ABOGADOS.png"
+        src="/ABOGADOS.png"
         alt="ABOGADOS"
         style={{ width: "100%", maxWidth: "800px", marginBottom: "2rem" }}
       />
       <p>Ejercicio Sistemas</p>
       <img
-        src="/public/SISTEMAS.png"
+        src="/SISTEMAS.png"
         alt="SISTEMAS"
         style={{ width: "100%", maxWidth: "800px", marginBottom: "2rem" }}
       />
       <p>Ejercicio Universidad</p>
       <img
-        src="/public/MER-UVI.png"
+        src="/MER-UVI.png"
         alt="MER-UVI"
         style={{ width: "100%", maxWidth: "800px", marginBottom: "2rem" }}
       />
